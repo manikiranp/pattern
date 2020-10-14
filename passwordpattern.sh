64c8b2055457469d6429dd3e98864808735ae516
manikiranp <manikiranp3@gmail.com>
2020-10-14 18:48:25 +0530

    inclusion of number condition

--- passwordpattern.sh
+++ passwordpattern.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#6
+#7
 
 echo "Password Rule 1: Minimum 8 characters"
 echo "Password Rule 2: Atleast 1 uppercase"
+echo "Password Rule 3: Atleast one number"
 read -p "Enter the password: " pass
 
-pattern="^(?=.*[A-Z])[a-zA-Z]{8,}$"
+pattern="^(?=.*[A-Z])(?=.*[0-9]).{8,}$"
 if [[ $pass =~ $pattern ]]; then
         echo True
 else
