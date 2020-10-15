2ccaf0c804ddd5000a85c218c953520353718afd
manikiranp <manikiranp3@gmail.com>
2020-10-15 14:15:24 +0530

    mobile number with countrycode

--- user_registration.sh
+++ user_registration.sh
@@ -29,7 +29,14 @@ then
 else
         echo False
 fi
- 
+#4
+read -p "Enter country code followed by space and phone number: " num
+pattern="^[1-9]{1}[0-9]{0,2} [1-9]{1}[0-9]{9}$"
+if [[ $num =~ $pattern ]]; then
+        echo True
+else
+        echo False
+fi 
 
 
 
