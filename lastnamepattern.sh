2b9138bc38cd4e0073ff4f97527cbcb0ec4fb258
manikiranp <manikiranp3@gmail.com>
2020-10-15 14:02:22 +0530

    last name pattern

--- lastnamepattern.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-#2
-read -p "Enter Last name: " last
-pattern="^([A-Z][a-z]{2,})$"
-if [[ $last =~ $pattern ]];  then
-        echo True
-else
-        echo False
-fi
--- user_registration.sh
+++ user_registration.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
 echo "Patterns: Day 14 assignment"
-
 #1
 read -p "Enter your First name: " first
 pattern="^([A-Z]){1}[a-zA-Z]{2,}$"
 if [[ $first =~ $pattern ]]; then
+        echo True
+else
+        echo False
+fi
+#2
+read -p "Enter your Last name: " last
+pattern="^([A-Z]){1}[a-zA-Z]{2,}$"
+if [[ $last =~ $pattern ]]; then
         echo True
 else
         echo False
